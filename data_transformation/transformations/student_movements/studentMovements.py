c6ca516d68e3f2f54ba357f400a0a03659b8a3d2
Thomas Ojeda <tojeda@alumnos.exa.unicen.edu.ar>
2023-04-15 19:52:37 -0300

    changes to test a python script execution

---
+++ data_transformation/transformations/student_movements/studentMovements.py
@@ -0,0 +1,5 @@
+
+
+from utils.select import selectColumns
+
+print(selectColumns(['UNIDAD','DOCUMENTO','CARRERA']))
--- routes/master/charts.js
+++ routes/master/charts.js
@@ -1,5 +1,6 @@
 const express = require("express");
-
+const {spawn} = require('child_process');
+const path = require('path')
 const {
   getAllCharts,
   getChart,
@@ -9,13 +10,36 @@ const {
   deleteAllCharts,
 } = require("../../controllers/master/chart");
 
+
 const chartsMasterRouter = express.Router();
 
-chartsMasterRouter.get("/" ,getAllCharts);
+const testpython = (req, res) => {
+
+  var dataToSend;
+  // spawn new child process to call the python script
+  const python = spawn("python", [path.resolve(__dirname+'/testfile.py')]);
+  // collect data from script
+  python.stdout.on("data", function (data) {
+
+    console.log("Pipe data from python script ...");
+    dataToSend = data.toString();
+  });
+  // in close event we are sure that stream from child process is closed
+  python.on("close", (code) => {
+    console.log(`child process close all stdio with code ${code}`);
+    // send data to browser
+    res.send(dataToSend);
+  });
+};
+chartsMasterRouter.get("/testpy",testpython);
+
+chartsMasterRouter.get("/", getAllCharts);
 chartsMasterRouter.get("/:id", getChart);
 chartsMasterRouter.post("/", createChart);
 chartsMasterRouter.patch("/:id", updateChart);
-chartsMasterRouter.delete("/:id",deleteChart)
-chartsMasterRouter.delete("/",deleteAllCharts)
+chartsMasterRouter.delete("/:id", deleteChart);
+chartsMasterRouter.delete("/", deleteAllCharts);
+
+
 
 module.exports = { chartsMasterRouter };

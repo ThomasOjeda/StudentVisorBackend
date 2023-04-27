a9258397c98bb7d7bc9830cae27d7c9b15516e8d
Thomas Ojeda <tojeda@alumnos.exa.unicen.edu.ar>
2023-04-27 15:37:59 -0300

    changed the way the docker compose files handle environment variables

--- config/config.js
+++ config/config.js
@@ -2,15 +2,12 @@ const MONGO_IP = process.env.MONGO_IP || "mongo";
 const MONGO_PORT = process.env.MONGO_PORT || 27017;
 const MONGO_USER = process.env.MONGO_USER;
 const MONGO_PASSWORD = process.env.MONGO_PASSWORD;
-const MONGO_URI =
-  process.env.MONGO_URI ||
-  `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/studentvisordb?retryWrites=true&w=majority&authSource=admin`;
+const MONGO_URI = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/studentvisordb?retryWrites=true&w=majority&authSource=admin`;
 
 const PORT = process.env.PORT || 5000;
 
-const JWT_SECRET = process.env.JWT_SECRET || "B&E)H@McQfTjWnZr4u7x!A%C*F-JaNd";
-const MASTER_SECRET =
-  process.env.MASTER_SECRET || "MASTER_SECRET9yB?E(H+MbQeThWmZq4t7w!z%C*F)J@";
+const JWT_SECRET = process.env.JWT_SECRET;
+const MASTER_SECRET = process.env.MASTER_SECRET;
 
 module.exports = {
   MONGO_IP,

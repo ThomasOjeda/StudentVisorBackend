9304e44ccaa31c4bfff73d56c7775488a1119b2d
ThomasOjeda <thomasojeda05@gmail.com>
2023-07-17 00:17:01 -0300

    Modified the transformation controllers to allow a previsualization of the generated chart without storing it in the database using the aid of middleware to partition the code in a sensible way

--- server_node/apimaster/controllers/transformation-main-handler.js
+++ server_node/apimaster/controllers/transformation-main-handler.js
@@ -1,13 +1,13 @@
 const { BadRequest, NotFound } = require("../../errors/errors-index");
 const TransformationType = require("../../models/transformation-types");
-const checkValidAndDuplicateTags = require("../../utils/check-valid-and-duplicate-tags");
-
+/* const checkValidAndDuplicateTags = require("../../utils/check-valid-and-duplicate-tags");
+ */
 const handlers = {
   [TransformationType.STMV]: require("./transformation-handlers/transform-movements-handler"),
   [TransformationType.INSC]: require("./transformation-handlers/transform-inscriptions-handler"),
 };
 
-const transformationHandler = async (req, res) => {
+const transformationHandler = async (req, res, next) => {
   if (!req.body.transformationHeader)
     throw new BadRequest("Transformation header not found in request");
 
@@ -20,19 +20,11 @@ const transformationHandler = async (req, res) => {
   if (!req.body.transformationHeader.name)
     throw new BadRequest("Name not found in transformation header");
 
-  //Check if there is specified tags for this transformation
-  if (req.body.transformationHeader.tags)
-    if (req.body.transformationHeader.tags.length > 0)
-      req.body.transformationHeader.tags = await checkValidAndDuplicateTags(
-        req.body.transformationHeader.tags
-      );
-    else req.body.transformationHeader.tags = [];
-
   const handler = handlers[req.body.transformationHeader.type];
   if (handler == undefined)
     throw new NotFound("Transformation type is invalid");
 
-  await handler(req, res);
+  await handler(req, res, next);
 };
 
 module.exports = transformationHandler;
---
+++ server_node/apimaster/middleware/transformations-middleware/check-tags.js
@@ -0,0 +1,13 @@
+const checkTags = async (req, res, next) => {
+  //Check if there is specified tags for this transformation
+  if (req.body.transformationHeader && req.body.transformationHeader.tags)
+    if (req.body.transformationHeader.tags.length > 0)
+      req.body.transformationHeader.tags = await checkValidAndDuplicateTags(
+        req.body.transformationHeader.tags
+      );
+    else req.body.transformationHeader.tags = [];
+
+  next();
+};
+
+module.exports = checkTags;

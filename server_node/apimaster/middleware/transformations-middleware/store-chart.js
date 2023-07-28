cdee2ae5ae20b3ef61789d1226a8bcc16dea3db5
ThomasOjeda <thomasojeda05@gmail.com>
2023-07-28 18:16:19 -0300

    rPrfrRQR added a new trasformations middleware that handles the creation of the previsualization object

--- server_node/apimaster/middleware/transformations-middleware/store-chart.js
+++ server_node/apimaster/middleware/transformations-middleware/store-chart.js
@@ -1,12 +1,8 @@
 const chart = require("../../../models/chart");
 
 const storeChart = async (req, res, next) => {
-  await chart.create({
-    name: req.body.transformationHeader.name,
-    type: req.body.transformationHeader.type,
-    tags: req.body.transformationHeader.tags,
-    structure: req.body.calculatedResult,
-  });
+  let newChart = await chart.create(req.chartObject);
+  req.chartObject = newChart;
   next();
 };
 
--- server_node/apimaster/routes/transformation.js
+++ server_node/apimaster/routes/transformation.js
@@ -5,6 +5,7 @@ const transformationHandler = require("../controllers/transformation-main-handle
 const checkTags = require("../middleware/transformations-middleware/check-tags");
 const storeChart = require("../middleware/transformations-middleware/store-chart");
 const sendChart = require("../middleware/transformations-middleware/send-chart");
+const createChartObject = require("../middleware/transformations-middleware/create-chart-object");
 
 const transformationsMasterRouter = express.Router();
 
@@ -12,9 +13,15 @@ transformationsMasterRouter.post(
   "/",
   checkTags,
   transformationHandler,
+  createChartObject,
   storeChart,
   sendChart
 );
-transformationsMasterRouter.post("/pre", transformationHandler, sendChart);
+transformationsMasterRouter.post(
+  "/pre",
+  transformationHandler,
+  createChartObject,
+  sendChart
+);
 
 module.exports = transformationsMasterRouter;

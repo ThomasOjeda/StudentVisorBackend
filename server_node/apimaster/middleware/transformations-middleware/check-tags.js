ef7e130a9904d8d526a2e9e25c5a83fbdfb6c6cd
ThomasOjeda <thomasojeda05@gmail.com>
2023-07-29 18:23:17 -0300

    fixed a missing import

--- server_node/apimaster/middleware/transformations-middleware/check-tags.js
+++ server_node/apimaster/middleware/transformations-middleware/check-tags.js
@@ -1,3 +1,5 @@
+const checkValidAndDuplicateTags = require("../../../utils/check-valid-and-duplicate-tags");
+
 const checkTags = async (req, res, next) => {
   //Check if there is specified tags for this transformation
   if (req.body.transformationHeader && req.body.transformationHeader.tags)

47a39eb3e99ba99d446655e620240c24718ab594
ThomasOjeda <thomasojeda05@gmail.com>
2023-09-03 18:21:54 -0300

    poHHa5g9 El registro de usuarios esta disponible solo para el rol admin

--- server_node/apiv1/routes/auth.js
+++ server_node/apiv1/routes/auth.js
@@ -5,9 +5,12 @@ const {
   register,
 } = require("../../apimaster/controllers/authentication");
 
+const authentication = require("../middleware/authentication");
+const authorization = require("../middleware/authorization");
+
 const authRouter = express.Router();
 
 authRouter.post("/login", login);
-authRouter.post("/register", register);
+authRouter.post("/register", authentication, authorization("admin"), register);
 
 module.exports = authRouter;

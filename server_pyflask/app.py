53d64792f22071755b989275306f23c8c9af5d6f
ThomasOjeda <thomasojeda05@gmail.com>
2023-09-20 16:30:58 -0300

    Z1vsB6Xu __init__.py  junto a app.py no permitia que waitress encontrara el objeto del servidor

--- server_pyflask/app.py
+++ server_pyflask/app.py
@@ -1,6 +1,4 @@
-import os
 from flask import Flask
-
 import logging
 
 
@@ -8,6 +6,6 @@ logging.basicConfig(level=logging.DEBUG, format="%(asctime)s %(message)s")
 
 app = Flask(__name__)
 
-from .routes.routes import routesBP
+from src.routes.routes import routesBP
 
 app.register_blueprint(routesBP, url_prefix="/")

10355c52719450c2976ba051db55a78265154c15
ThomasOjeda <tojeda@alumnos.exa.unicen.edu.ar>
2023-10-07 18:36:22 -0300

    j02GhwkO Transformacion de inscripciones a unidades ya no considera inscripciones duplicadas

--- server_pyflask/src/transformations/units_inscriptions.py
+++ server_pyflask/src/transformations/units_inscriptions.py
@@ -6,5 +6,8 @@ from ..utils.enums import ColName
 
 class UnitInscriptions(Transformation):
     def transform(self, enrollments) -> pd.DataFrame:
+        enrollments = enrollments.drop_duplicates(
+            subset=[ColName.ID.value, ColName.UNIT.value]
+        )
         enrollments = enrollments.groupby(ColName.UNIT.value).count()
         return enrollments[ColName.ID.value]

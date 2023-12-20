42c200ee5c929a20c67df6f52de9def20587f63a
ThomasOjeda <tojeda@alumnos.exa.unicen.edu.ar>
2023-12-20 16:59:25 -0300

    EvrrObT6 reemplazo de normalizadores de ofertas de inscripciones a reemplazo por listas

--- server_pyflask/src/utils/normalizers.py
+++ server_pyflask/src/utils/normalizers.py
@@ -102,25 +102,12 @@ def offerNamesReplacing(df: pd.DataFrame) -> pd.DataFrame:
     return df
 
 
-inscriptionReplacements = {
-    "reinscripto": "r",
-    "aspirante a propuesta": "a",
-    "inscripcion a propuesta aceptada": "i",
-}
-
-
 def inscriptionTypeNormalization(df: pd.DataFrame) -> pd.DataFrame:
     df[ColName.INSC_TYPE.value] = df[ColName.INSC_TYPE.value].replace(
-        inscriptionReplacements, regex=True
+        ["reinscripto", "aspirante a propuesta", "inscripcion a propuesta aceptada"],
+        ["r", "a", "i"],
     )
     return df
-
-
-studentInscriptionsOfferPatches = {
-    "convenio ingenieria sistemas": "ingenieria de sistemas",
-    "licenciatura en antropologia orientacion antropologia social": "licenciatura en antropologia",
-    "licenciatura en antropologia orientacion arqueologia": "licenciatura en antropologia",
-}
 
 
 def studentInscriptionsOfferNormalization(df: pd.DataFrame) -> pd.DataFrame:
@@ -129,36 +116,45 @@ def studentInscriptionsOfferNormalization(df: pd.DataFrame) -> pd.DataFrame:
     ] = "facultad de ciencias exactas"
 
     df[ColName.OFFER.value] = df[ColName.OFFER.value].replace(
-        studentInscriptionsOfferPatches
+        [
+            "convenio ingenieria sistemas",
+            "licenciatura en antropologia orientacion antropologia social",
+            "licenciatura en antropologia orientacion arqueologia",
+        ],
+        [
+            "ingenieria de sistemas",
+            "licenciatura en antropologia",
+            "licenciatura en antropologia",
+        ],
     )
 
     return df
 
 
 #########
-scholarshipsOffersTranslations = {
-    "Ingeniero Agronomo": "Ingenieria Agronomica",
-    "Licenciado en Administracion Agraria": "Licenciatura en Administracion Agraria",
-    "Licenciado en Tecnologia de los Alimentos - Mencion Produccion de Materia Prima de Origen Vegetal": "Licenciatura en Tecnologia de los Alimentos",
-    "Licenciado en Tecnologia de los Alimentos - Mencion Industrializacion de Alimentos de Origen Vegetal": "Licenciatura en Tecnologia de los Alimentos",
-    "Licenciado en Tecnologia de los Alimentos - Mencion Tecnologia de los Alimentos de Origen Animal": "Licenciatura en Tecnologia de los Alimentos",
-    "Profesor en Ciencias Biologicas": "Profesorado en Ciencias Biologicas",
-    "Ingeniero de Sistemas": "Ingenieria de Sistemas",
-    "Ingeniero Agrimensor": "Ingenieria en Agrimensura",
-    "Ingeniero Civil": "Ingenieria Civil",
-    "Ingeniero Electromecanico": "Ingenieria Electromecanica",
-    "Ingeniero Industrial": "Ingenieria Industrial",
-    "Ingeniero Quimico": "Ingenieria Quimica",
-    "Licenciado en Ciencias Fisicas": "Licenciatura en Ciencias Fisicas",
-    "Licenciado en Ciencias Matematicas": "Licenciatura en Ciencias Matematicas",
-    "Licenciado en Diagnostico y Gestion Ambiental": "Licenciatura en Diagnostico y Gestion Ambiental",
-    "Licenciado en Geografia": "Licenciatura en Geografia",
-    "Licenciado en Logistica Integral": "Licenciatura en Logistica Integral",
-    "Licenciado en Tecnologia Ambiental": "Licenciatura en Tecnologia Ambiental",
-    "Medico Veterinario": "Medicina Veterinaria",
-    "Profesor de Matematica": "Profesorado de Matematica",
-    "Profesor en Ciencias Biologicas": "Profesorado en Ciencias Biologicas",
-    "Tecnico Universitario en Desarrollo de Aplicaciones Informaticas": "Tecnicatura Universitaria en Desarrollo de Aplicaciones Informaticas",
-    "Tecnico Universitario en Programacion y Administracion de Redes": "Tecnicatura Universitaria en Programacion y Administracion de Redes",
-    "Tecnico/a Universitario/a en Administracion de Redes Informaticas": "Tecnicatura Universitaria en Administracion de Redes Informaticas",
-}
+# scholarshipsOffersTranslations = {
+#     "Ingeniero Agronomo": "Ingenieria Agronomica",
+#     "Licenciado en Administracion Agraria": "Licenciatura en Administracion Agraria",
+#     "Licenciado en Tecnologia de los Alimentos - Mencion Produccion de Materia Prima de Origen Vegetal": "Licenciatura en Tecnologia de los Alimentos",
+#     "Licenciado en Tecnologia de los Alimentos - Mencion Industrializacion de Alimentos de Origen Vegetal": "Licenciatura en Tecnologia de los Alimentos",
+#     "Licenciado en Tecnologia de los Alimentos - Mencion Tecnologia de los Alimentos de Origen Animal": "Licenciatura en Tecnologia de los Alimentos",
+#     "Profesor en Ciencias Biologicas": "Profesorado en Ciencias Biologicas",
+#     "Ingeniero de Sistemas": "Ingenieria de Sistemas",
+#     "Ingeniero Agrimensor": "Ingenieria en Agrimensura",
+#     "Ingeniero Civil": "Ingenieria Civil",
+#     "Ingeniero Electromecanico": "Ingenieria Electromecanica",
+#     "Ingeniero Industrial": "Ingenieria Industrial",
+#     "Ingeniero Quimico": "Ingenieria Quimica",
+#     "Licenciado en Ciencias Fisicas": "Licenciatura en Ciencias Fisicas",
+#     "Licenciado en Ciencias Matematicas": "Licenciatura en Ciencias Matematicas",
+#     "Licenciado en Diagnostico y Gestion Ambiental": "Licenciatura en Diagnostico y Gestion Ambiental",
+#     "Licenciado en Geografia": "Licenciatura en Geografia",
+#     "Licenciado en Logistica Integral": "Licenciatura en Logistica Integral",
+#     "Licenciado en Tecnologia Ambiental": "Licenciatura en Tecnologia Ambiental",
+#     "Medico Veterinario": "Medicina Veterinaria",
+#     "Profesor de Matematica": "Profesorado de Matematica",
+#     "Profesor en Ciencias Biologicas": "Profesorado en Ciencias Biologicas",
+#     "Tecnico Universitario en Desarrollo de Aplicaciones Informaticas": "Tecnicatura Universitaria en Desarrollo de Aplicaciones Informaticas",
+#     "Tecnico Universitario en Programacion y Administracion de Redes": "Tecnicatura Universitaria en Programacion y Administracion de Redes",
+#     "Tecnico/a Universitario/a en Administracion de Redes Informaticas": "Tecnicatura Universitaria en Administracion de Redes Informaticas",
+# }
